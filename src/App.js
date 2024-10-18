bb3deed7e5822067c94b9baf17161794df10bfc6
texalana <79461309+texalana@users.noreply.github.com>
2024-10-18 17:17:14 +0200

    Added city name and image to page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import destinations from "./destinations.json";
+import { useState } from "react";
 
 function App() {
+  const [destination, setDestination] = useState([destinations]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Travel bucket list</h2>
+      {destinations.map((city) => (
+        <div>
+          <p>{city.name}</p>
+          <img src={city.image} />
+        </div>
+      ))}
+    </>
   );
 }
 
