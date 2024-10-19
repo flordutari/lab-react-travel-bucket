6bfbf9d0a89983f3850ebe5bcffa2f1adf2ba64f
Grace Vivian <gracevivian04@gmail.com>
2024-10-19 15:17:09 +0200

    finished challenges in README. Added 'fake' data in json file, created DestinationBox component with requested functionalities including visited toggle button and delete button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import destinations from "./destinations.json";
+import DestinationBox from './components/DestinationBox';
 
 function App() {
+  console.log(destinations);
+  const [locations, setLocations] = useState(destinations.destinations || []);
+
+  const removeDestination = (index) => {
+    let newLocations = [...locations];
+    newLocations.splice(index, 1);
+    setLocations(newLocations)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ul>
+      {locations.map((location, index) => (
+        <li key={index}>
+          {location.name} {" "} 
+          <img src={location.image} width={150} alt={location.name} />
+        </li>
+      ))}
+      </ul> */}
+      <h1>
+        Explore Destinations
+      </h1>
+      <div>
+        {locations.map((location, index) => (
+          <DestinationBox key={index} destination={location} onDelete={(index) => removeDestination(index)} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+  // useEffect(() => {
+  //   fetch('./destinations.json')
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     setLocations(data)
+  //   })
+  //   .catch((error) => console.error("Error loading json", error))
+  // }, []);
