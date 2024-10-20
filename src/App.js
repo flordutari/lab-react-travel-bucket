34db0fa484fab8a0570d908e9ec004afbb2ab8c2
SiobhanWhyte <77633144+SiobhanWhyte@users.noreply.github.com>
2024-10-20 10:34:59 +0200

    iteration 4, added functionality to mark destination as visited.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import DestinationBox from "./assets/destinationBox";
 function App() {
   const [destinationList, setDestinationList] = useState(destinations);
 
+  const markAsVisitied = (name) => {
+    const updatedDestinations = destinationList.map((destination) =>
+      destination.name == name ? { ...destination, visted: true } : destination
+    );
+    setDestinationList(updatedDestinations);
+  };
+
   return (
     <div className="App">
       {destinationList.map((destination, index) => (
-        <DestinationBox key={index} destination={destination} />
+        <DestinationBox
+          key={index}
+          destination={destination}
+          markAsVisitied={markAsVisitied}
+        />
       ))}
     </div>
   );
--- src/assets/destinationBox.js
+++ src/assets/destinationBox.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-function DestinationBox({ destination }) {
+function DestinationBox({ destination, markAsVisitied }) {
   return (
     <div>
       <h2>{destination.name}</h2>
       <img src={destination.image} alt={destination.name} width={150} />
       <p>
+        <strong>Continent:</strong>
+        {destination.continent}
+      </p>
+      <p>
         <strong>Notes:</strong>
         {destination.notes}
       </p>
-      <button>Mark as Visited</button>
+      <button onClick={() => markAsVisitied(destination.name)}>
+        {destination.vistited ? "Visited" : "Mark as Visited"}
+      </button>
     </div>
   );
 }
