e9020d0981b315b6d2a11c3e5643b9223633d94e
texalana <79461309+texalana@users.noreply.github.com>
2024-10-21 15:37:27 +0200

    Added delete city button and functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import { useState } from "react";
 import DestinationBox from "./components/DestinationBox";
 
 function App() {
-  const [destination, setDestination] = useState([destinations]);
+  const [destination, setDestination] = useState(destinations);
 
+  function deleteCity(deleteCity) {
+    console.log(deleteCity);
+    const newCityList = destination.filter((city) => city !== deleteCity);
+    setDestination(newCityList);
+  }
   return (
     <>
-      {/* {console.log(destinations)} */}
-      <DestinationBox cities={destinations} />
+      {console.log(destination)}
+      <DestinationBox
+        cities={destination}
+        onDeleteCity={(city) => deleteCity(city)}
+      />
     </>
   );
 }
