753d3ec6ef7ac4a02df62e0fa25abba3d1ca2570
Desirée <lagerquistd@yahoo.com>
2024-10-21 16:57:11 +0200

    done; pending form to add destinations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { useState } from 'react'
 import destinations from './destinations.json';
 import './App.css';
+import DestinationBox from './components/DestinationBox';
+
 
 export default function App() {
 
   const [destinationData, setDestinationData] = useState(destinations)
 
+  function toggleVisited(name) {
+    setDestinationData(destinationData.map(destination => 
+      destination.name === name 
+      ? {...destination, visited: !destination.visited } 
+      : destination 
+    ))
+  }
+
+  function deleteDestination(name) {
+    setDestinationData(destinationData.filter(destination => destination.name !== name))
+  }
 
   return (
-    <div className="App">
-      <ul>
-        {destinationData.map(destination => (
-          <li>
-            <p>{destination.name}</p>
-            <img src={destination.image} style={{width: '100px'}} />
-          </li>
-        ))}
-      </ul>
+    <div className='App'>
+      <h1>TRAVEL BUCKET LIST</h1>
+      <DestinationBox destinations={destinationData} toggleVisited={(name) => toggleVisited(name)} deleteDestination={deleteDestination}/>
     </div>
   );
 }
--- src/components/DestinationBox.js
+++ src/components/DestinationBox.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './DestinationBox.css';
 
-export default function DestinationBox() {
-    
-  return (
-    <div>DestinationBox</div>
-  )
+export default function DestinationBox({destinations, toggleVisited, deleteDestination}) {
+
+    return (
+    <div>
+        <ul>
+        {destinations.map(destination => (
+            <li key={destination.name}
+            className={destination.visited === true ? 'visited' : 'notVisited'}>
+            <p id='locName'>{destination.name.toUpperCase()}</p>
+            <img src={destination.image} />
+            <p className='mediumPTag'>Continent: {destination.continent}</p>
+            <p className='mediumPTag'>Suggestions: {destination.notes}</p>
+            <p>
+                <input 
+                type='checkbox' 
+                checked={destination.visited} 
+                onChange={() => toggleVisited(destination.name)} 
+                /> 
+                Mark as visited
+            </p>
+            <button id='deletebtn' onClick={() => deleteDestination(destination.name)}>Delete Destination</button>
+            </li>
+        ))}
+        </ul>
+    </div>
+    )
 }
