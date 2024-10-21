da2b731b8118e7372ed7133f510bc1d08d911ccb
texalana <79461309+texalana@users.noreply.github.com>
2024-10-21 14:22:05 +0200

    Added mark as visited button and functionality

--- src/components/DestinationBox.js
+++ src/components/DestinationBox.js
@@ -1,18 +1,28 @@
 import React from "react";
 
 export default function DestinationBox({ cities }) {
+  function handleMarkAsVisited(city) {
+    console.log(city);
+    console.log(city.visited);
+    const haveVisited = !city.visited;
+    city.visited = haveVisited;
+    console.log(city.visited);
+  }
+
   return (
     <>
       {console.log(cities)}
 
       <h2>Travel bucket list</h2>
-      {cities.map((city) => (
+      {cities.map((city, index) => (
         <div>
-          <p>{city.name}</p>
-          <img className="city-img" src={city.image} />
-          <p>{city.continent}</p>
-          <p>{city.notes}</p>
-          <button>Mark as visited</button>
+          <p key={city.name}>{city.name}</p>
+          <img key={city.img} className="city-img" src={city.image} />
+          <p key={city.continent}>{city.continent}</p>
+          <p key={city.notes}>{city.notes}</p>
+          <button onClick={() => handleMarkAsVisited(city)}>
+            Mark as visited
+          </button>
         </div>
       ))}
     </>
