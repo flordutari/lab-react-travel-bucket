37707351de529cbec9ec0ffa6ba43d0b32bf5fdb
VanyaMark <markova.vanya@gmail.com>
2024-10-24 16:51:20 +0200

    done map on app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,78 @@ import NewDestinationForm from "./components/NewDestinationForm";
 function App() {
   // eslint-disable-next-line no-use-before-define
   const [destinationsList, setDestinationsList] = useState(destinations);
-  const [input, setInput] = useState({name:"", image:"", continent:"", visited: false, notes:""})
+  const [input, setInput] = useState({
+    name: "",
+    image: "",
+    continent: "",
+    visited: false,
+    notes: "",
+  });
 
   const handleVisitedBtn = (index) => {
-    setDestinationsList((prevDestinationsList)=> {
-      const newDestinationsList = [...prevDestinationsList]
+    setDestinationsList((prevDestinationsList) => {
+      const newDestinationsList = [...prevDestinationsList];
       newDestinationsList[index].visited = true;
-      const filteredDestinationsList = newDestinationsList.filter(destination => !destination.visited)
-      console.log(filteredDestinationsList)
-      return filteredDestinationsList
-    })
-  }
+      const filteredDestinationsList = newDestinationsList.filter(
+        (destination) => !destination.visited
+      );
+      console.log(filteredDestinationsList);
+      return filteredDestinationsList;
+    });
+  };
 
   const handleDeleteBtn = (index) => {
-    setDestinationsList((prevDestinationsList)=>{
-
-      const newDestinationsList = [...prevDestinationsList]
-      newDestinationsList.splice(index, 1)
-      console.log(newDestinationsList)
-      return newDestinationsList
-    })
-  }
+    setDestinationsList((prevDestinationsList) => {
+      const newDestinationsList = [...prevDestinationsList];
+      newDestinationsList.splice(index, 1);
+      console.log(newDestinationsList);
+      return newDestinationsList;
+    });
+  };
 
   function handleChange(event) {
     console.log(event.target.name);
     //01-create a copy of the input object
-    const newInput = {...input};
+    const newInput = { ...input };
     //02-change the object
-    newInput[event.target.name] = event.target.value
+    newInput[event.target.name] = event.target.value;
     //03-update the state
-    setInput(newInput)
+    setInput(newInput);
   }
-  
+
   const handleSubmit = (e) => {
     //01-prevent the default behaviour
-    e.preventDefault()
+    e.preventDefault();
     //02-Modify the state
-    const newDestinationsList = [...destinationsList]
-    newDestinationsList.push(input)
+    const newDestinationsList = [...destinationsList];
+    newDestinationsList.push(input);
     //03-Update destination list
-    setDestinationsList(newDestinationsList)
+    setDestinationsList(newDestinationsList);
     //04-Reset input
-    setInput({name: "", image: "", continent: "", visited: false, notes: ""})
-  }
+    setInput({ name: "", image: "", continent: "", visited: false, notes: "" });
+  };
 
   return (
     <div className="App">
       <main>
-        <DestinationBox destinationsList= {destinationsList} handleVisitedBtn={(index)=> handleVisitedBtn(index)} handleDeleteBtn={(index)=>handleDeleteBtn(index)}></DestinationBox>
-        <NewDestinationForm handleChange={handleChange} handleSubmit={handleSubmit} input={input}></NewDestinationForm>
+        <div className="container">
+          {destinationsList.map((destination, index) => {
+            return (
+              <DestinationBox
+                destination={destination}
+                index={index}
+                destinationsList={destinationsList}
+                handleVisitedBtn={(index) => handleVisitedBtn(index)}
+                handleDeleteBtn={(index) => handleDeleteBtn(index)}
+              />
+            );
+          })}
+        </div>
+        <NewDestinationForm
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          input={input}
+        ></NewDestinationForm>
         {/* <form onSubmit={handleSubmit}>
             <input
               value={input.name}
--- src/components/DestinationBox.jsx
+++ src/components/DestinationBox.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import "../App.css"
 
-const DestinationBox = ({destinationsList, handleVisitedBtn, handleDeleteBtn}) => {
+const DestinationBox = ({destination, index, handleVisitedBtn, handleDeleteBtn}) => {
   return (
-    <div className="container" >
-    {destinationsList.map((destination, index) => {
-      // console.log("from destinations json: ", destination)
-      return (
+
         <div className="card" key={index}>
           <p>{destination.name}</p>
           <img src={destination.image} alt="destination" width={200} />
           <button onClick={()=>handleVisitedBtn(index)} >Visited</button>
           <button onClick={()=> handleDeleteBtn(index)} >Delete</button>
         </div>
-      );
-    })}
-  </div>
   )
 }
 
